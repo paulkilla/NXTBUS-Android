2dac1879ce630d8496152b22c0c32cbaf4685383
Paul <pkil@2327222.no-reply.drupal.org>
2014-12-16 09:39:03 +1100

    Sample working request

--- src/main/java/au/com/pazzul/nxtbus/NxtBusActivity.java
+++ src/main/java/au/com/pazzul/nxtbus/NxtBusActivity.java
@@ -2,9 +2,18 @@ package au.com.pazzul.nxtbus;
 
 import android.app.Activity;
 import android.os.Bundle;
-import edu.usf.cutr.siri.android.client.SiriRestClient;
-import edu.usf.cutr.siri.android.client.config.SiriRestClientConfig;
-import uk.org.siri.siri.Siri;
+import android.os.StrictMode;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 
 public class NxtBusActivity extends Activity {
     /**
@@ -15,55 +24,75 @@ public class NxtBusActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        //Client object from this library that will make the request to the server
-        SiriRestClient client;
-
-// Setup server config
-        SiriRestClientConfig config = new SiriRestClientConfig(SiriRestClientConfig.RESPONSE_TYPE_JSON);  //Request JSON response
-        config.setHttpConnectionType(SiriRestClientConfig.HTTP_CONNECTION_TYPE_ANDROID);  //Request using Android HTTP connection object
-        config.setJacksonObjectType(SiriRestClientConfig.JACKSON_OBJECT_TYPE_READER);  //Request using the Jackson ObjectReader, which is more efficient than ObjectMapper
-
-// Instantiate client with URLs for server and config
-        //http://siri.nxtbus.act.gov.au:11000/requestorcode/sm/status.xml
-        client = new SiriRestClient(
-                "http://siri.nxtbus.act.gov.au:11000/B5233/vm/status.xml",
-                "http://siri.nxtbus.act.gov.au:11000/api/siri/stop-monitoring", config);
-
-        //Get the parameters that will be used to make the request - fill in these with your own info
-        String key = getDeveloperKey(); //Get the developer API key to use, perhaps from a config file
-        String operatorRef = getOperatorRef();  //Get the operator we're requesting info for
-        String monitoringRef = getStopId();  //Get the stop ID we're requesting info for, perhaps from UI field
-        String lineRef = getRouteId();  //Get the route ID filter, perhaps from UI field
-        int directionRefInt = getDirectionInt(); //Route direction filter (0 or 1)
-        String stopMonitoringDetailLevel = "normal";  //Get arrivals up to the specified stop ID (not after)
-        int maximumNumberOfCallsOnwardsInt = 3; //Get next 3 arrivals at this stop
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
 //Make request to the server, and get response data in a SIRI object
-        Siri siri = client.makeStopMonRequest(key,
-                operatorRef,
-                monitoringRef,
-                lineRef,
-                directionRefInt,
-                stopMonitoringDetailLevel,
-                maximumNumberOfCallsOnwardsInt);
-        System.out.println("Test: " + siri.getServiceDelivery().getResponseTimestamp().toString());
+        try {
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://siri.nxtbus.act.gov.au:11000/B5233/vm/status.xml");
+
+            try {
+                StringEntity se = new StringEntity( "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" standalone=\"yes\"?>\n" +
+                        "\n" +
+                        "<Siri version=\"2.0\" xmlns:ns2=\"http://www.ifopt.org.uk/acsb\" xmlns=\"http://www.sir\n" +
+                        "\n" +
+                        "i.org.uk/siri\" xmlns:ns4=\"http://datex2.eu/schema/2_0RC1/2_0\" xmlns:ns3=\"http://ww\n" +
+                        "\n" +
+                        "w.ifopt.org.uk/ifopt\">\n" +
+                        "\n" +
+                        "<CheckStatusRequest>\n" +
+                        "\n" +
+                        "<RequestTimestamp>2013-12-29T14:09:41.000+01:00</RequestTimestamp>\n" +
+                        "\n" +
+                        "<RequestorRef>B5233</RequestorRef>\n" +
+                        "\n" +
+                        "</CheckStatusRequest>\n" +
+                        "\n" +
+                        "</Siri>", HTTP.UTF_8);
+
+                se.setContentType("text/xml");
+                httppost.setEntity(se);
+
+                HttpResponse httpresponse = httpclient.execute(httppost);
+                HttpEntity resEntity = httpresponse.getEntity();
+                //tvData.setText(EntityUtils.toString(resEntity));
+                String entity = EntityUtils.toString(resEntity);
+
+                System.out.println("It worked: " + entity);
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("Test: ");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     private String getDeveloperKey() {
-        return "";
+        return "B5233";
     }
 
     public String getOperatorRef() {
-        return "";
+        return "ACT";
     }
 
     public String getStopId() {
-        return "";
+        return "0200";
     }
 
     public String getRouteId() {
-        return "";
+        return "200";
     }
 
     public int getDirectionInt() {
