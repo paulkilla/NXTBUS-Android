6e5133ab20053360a1a33c2e7e4e88c4fdf729c4
Paul <pkil@2327222.no-reply.drupal.org>
2014-12-15 15:40:56 +1100

    Initial commit

---
+++ src/au/com/pazzul/nxtbus/NxtBusActivity.java
@@ -0,0 +1,15 @@
+package au.com.pazzul.nxtbus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NxtBusActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
