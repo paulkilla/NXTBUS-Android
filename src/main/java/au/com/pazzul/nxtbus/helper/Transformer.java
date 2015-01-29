c642dd09e1b66b06fc166eca82257a6e444e253b
paul.kilpatrick <paul.kilpatrick@finance.gov.au>
2015-01-29 23:55:06 +1100

    Some more tests

---
+++ src/main/java/au/com/pazzul/nxtbus/helper/Transformer.java
@@ -0,0 +1,32 @@
+package au.com.pazzul.nxtbus.helper;
+
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+/**
+ * Created by HOMEPC on 29/01/2015.
+ */
+public class Transformer {
+
+    public static String prettyFormat(String input) {
+        try {
+            Source xmlInput = new StreamSource(new StringReader(input));
+            StringWriter stringWriter = new StringWriter();
+            StreamResult xmlOutput = new StreamResult(stringWriter);
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            transformerFactory.setAttribute("indent-number", 2);
+            javax.xml.transform.Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(xmlInput, xmlOutput);
+            return xmlOutput.getWriter().toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e); // simple exception handling, please review it
+        }
+    }
+}
--- src/main/test/au/com/pazzul/nxtbus/Tester.java
+++ src/main/test/au/com/pazzul/nxtbus/Tester.java
@@ -1,7 +1,7 @@
 package au.com.pazzul.nxtbus;
 
-import android.os.Handler;
 import au.com.pazzul.nxtbus.helper.DateHelper;
+import au.com.pazzul.nxtbus.helper.Transformer;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -18,8 +18,7 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.Date;
 
 /**
  * Created by HOMEPC on 29/01/2015.
@@ -29,14 +28,11 @@ import java.util.TimerTask;
 public class Tester {
 
     @Test
-    public void test() {
-
-
+    public void testVehicleStatus() {
         Robolectric.runUiThreadTasksIncludingDelayedTasks();
         Robolectric.getFakeHttpLayer().interceptHttpRequests(false);
-        String entity = "";
-        try {
 
+        try {
             HttpClient httpclient = new DefaultHttpClient();
             HttpPost httppost = new HttpPost(Constants.URL_VEHICLE_STATUS);
 
@@ -56,14 +52,83 @@ public class Tester {
 
                 HttpResponse httpresponse = httpclient.execute(httppost);
                 HttpEntity resEntity = httpresponse.getEntity();
-                entity = EntityUtils.toString(resEntity);
+                String entity = EntityUtils.toString(resEntity);
 
                 System.out.println("It worked: " + entity);
             } catch (ClientProtocolException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("Test: ");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testTimetablePoll() {
+        Robolectric.runUiThreadTasksIncludingDelayedTasks();
+        Robolectric.getFakeHttpLayer().interceptHttpRequests(false);
+
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(Constants.URL_PROD_POLL);
+
+            try {
+                String time = DateHelper.generateRequestTimeStamp();
+                StringEntity se = new StringEntity(  "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" standalone=\"yes\"?>\n" +
+                        "\n" +
+                        "<Siri version=\"1.3\" xmlns:ns2=\"http://www.ifopt.org.uk/acsb\" xmlns=\"http://www.siri.org.uk/siri\" \n" +
+                        "\n" +
+                        "xmlns:ns4=\"http://datex2.eu/schema/2_0RC1/2_0\" xmlns:ns3=\"http://www.ifopt.org.uk/ifopt\">\n" +
+                        "\n" +
+                        "<ServiceRequest>\n" +
+                        "\n" +
+                        "<RequestTimestamp>" + time + "</RequestTimestamp>\n" +
+                        "\n" +
+                        "<RequestorRef>" + Constants.API_KEY + "</RequestorRef>\n" +
+                        "\n" +
+                        "<ProductionTimetableRequest version=\"1.3\">\n" +
+                        "\n" +
+                        "<RequestTimestamp>" + time + "</RequestTimestamp>\n" +
+                        "\n" +
+                        "<ValidityPeriod>\n" +
+                        "\n" +
+                        "<StartTime>2015-01-29T13:50:04+01:00</StartTime>\n" +
+                        "\n" +
+                        "<EndTime>2015-01-30T13:50:04+01:00</EndTime>\n" +
+                        "\n" +
+                        "</ValidityPeriod>\n" +
+                        "\n" +
+                        "<Lines>\n" +
+                        "\n" +
+                        "<LineDirection>\n" +
+                        "\n" +
+                        "<LineRef>ACT_200</LineRef>\n" +
+                        "\n" +
+                        "<DirectionRef>A</DirectionRef>\n" +
+                        "\n" +
+                        "<LineDirection>\n" +
+                        "\n" +
+                        "</Lines>\n" +
+                        "\n" +
+                        "</ProductionTimetableRequest>" +
+                        "</ServiceRequest>\n" +
+                        "\n" +
+                        "</Siri>", HTTP.UTF_8);
+
+                se.setContentType("text/xml");
+                httppost.setEntity(se);
+
+                HttpResponse httpresponse = httpclient.execute(httppost);
+                HttpEntity resEntity = httpresponse.getEntity();
+                String entity = EntityUtils.toString(resEntity);
+
+                System.out.println("It worked: " + Transformer.prettyFormat(entity));
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             System.out.println("Test: ");
